506694cfffaf15bd34042aeb18b165e1851378db
Felipe G. Hespanhol <felipeghespanhol@gmail.com>
2024-09-11 10:42:17 -0300

    Updates in nvim config

---
+++ nvim/lua/plugins/copilot-chat.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    branch = "canary",
+    dependencies = {
+      { "zbirenbaum/copilot.lua" }, -- or github/copilot.vim
+      { "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
+    },
+    build = "make tiktoken", -- Only on MacOS or Linux
+    opts = {
+      debug = true, -- Enable debugging
+      model = "gpt-4",
+      temperature = 0.1, -- default 0.1
+      -- See Configuration section for rest
+    },
+    -- See Commands section for default commands if you want to lazy load on them
+  },
+}
