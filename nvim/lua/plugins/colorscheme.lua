f1c86f9e3aca7800edd66b1d0b2b170ffe5ad2d8
Felipe G. Hespanhol <felipe.hespanhol@patientiq.io>
2025-10-08 14:56:42 -0300

    Add MacOS config in .tmux.conf; Add adwaita theme in neovim

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,18 +1,33 @@
--- return {
---   "ellisonleao/gruvbox.nvim",
---   priority = 1000,
---   opts = ...,
---   config = function()
---     vim.o.background = "dark"
---     -- vim.o.background = "light"
---     vim.cmd([[colorscheme gruvbox]])
---   end
--- }
-return {
-  "Mofiqul/vscode.nvim",
-  priority = 1000,
-  opts = ...,
-  config = function()
-    vim.cmd.colorscheme "vscode"
-  end
+ return {
+   -- {
+   --   "ellisonleao/gruvbox.nvim",
+   --   priority = 1000,
+   --   opts = ...,
+   --   config = function()
+   --     vim.o.background = "dark"
+   --     -- vim.o.background = "light"
+   --     vim.cmd([[colorscheme gruvbox]])
+   --   end
+   -- },
+   {
+     "Mofiqul/vscode.nvim",
+     priority = 1000,
+     opts = ...,
+     config = function()
+       vim.cmd.colorscheme "vscode"
+     end
+   },
+   -- {
+   --   "Mofiqul/adwaita.nvim",
+   --   lazy = false,
+   --   priority = 1000,
+
+   --   -- configure and set on startup
+   --   config = function()
+   --       vim.g.adwaita_darker = false             -- for darker version
+   --       vim.g.adwaita_disable_cursorline = true -- to disable cursorline
+   --       vim.g.adwaita_transparent = true        -- makes the background transparent
+   --       vim.cmd('colorscheme adwaita')
+   --   end
+   -- }
 }
